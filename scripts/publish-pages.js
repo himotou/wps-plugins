50df0673ea51a986911c86ddddcb03ac66cd5191
zhangchong <sazjc@sina.com>
2026-06-18 16:24:27 +0800

    test install

--- scripts/publish-pages.js
+++ scripts/publish-pages.js
@@ -52,68 +52,6 @@ function createPublishPage() {
     /SERVERID_REPLEASE_STRING/,
     enableMultiUser ? 'getServerId()' : 'undefined'
   );
-  publishHtml = publishHtml.replace(
-    /function LoadAddons\(\) \{([\s\S]*?)InitSdk\(\);([\s\S]*?)\}/,
-    `function LoadAddons() {$1LoadPublishAddons();
-            InitSdk();$2}`
-  );
-  publishHtml = publishHtml.replace(
-    /function CheckPlugin\(element\) \{[\s\S]*?function GetAddonId\(element\) \{/,
-    `function CheckPlugin(element) {
-            var id = GetAddonId(element);
-            var ele = document.getElementById(id + "_status");
-            var offline = element.online === "false";
-            var url = offline ? element.url : element.url + "ribbon.xml";
-            var xmlReq = getHttpObj();
-            xmlReq.open("GET", url);
-            xmlReq.onload = function (res) {
-                var response = IEVersion() < 10 ? xmlReq.responseText : res.target.response;
-                if ((offline && response.startsWith("7z"))
-                    || !offline && response.startsWith("<customUI")) {
-                    ele.style.color = "green";
-                    ele.style.textAlign = "center";
-                    ele.innerHTML = "正常";
-                } else {
-                    ele.style.color = "white";
-                    ele.style.backgroundColor = "gray";
-                    ele.style.textAlign = "center";
-                    ele.innerHTML = "无效";
-                    ele.title = offline ? ("不是有效的7z格式" + url) : ("不是有效的ribbon.xml，" + url);
-                }
-            }
-            xmlReq.onerror = function () {
-                ele.style.color = "white";
-                ele.style.backgroundColor = "gray";
-                ele.style.textAlign = "center";
-                ele.innerHTML = "无效";
-                ele.title = "网页路径不可访问：" + url;
-            }
-            xmlReq.ontimeout = function () {
-                ele.style.color = "white";
-                ele.style.backgroundColor = "gray";
-                ele.style.textAlign = "center";
-                ele.innerHTML = "异常";
-                ele.title = "访问超时，" + url;
-            }
-            if (IEVersion() < 10) {
-                xmlReq.onreadystatechange = function () {
-                    if (xmlReq.readyState != 4)
-                        return;
-                    if (xmlReq.bTimeout) {
-                        return;
-                    }
-                    if (xmlReq.status === 200)
-                        xmlReq.onload();
-                    else
-                        xmlReq.onerror();
-                }
-            }
-            xmlReq.timeout = 5000;
-            xmlReq.send(null);
-        }
-
-        function GetAddonId(element) {`
-  );
 
   fs.writeFileSync(path.join(publishRoot, 'publish.html'), publishHtml);
 }
